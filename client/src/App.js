312d05925ee52d7143f3d23c64b9fabc0f082b1b
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-17 00:40:57 -0500

    addressing more specific aspects of css

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ function App() {
   </div>
 
   <div class="columns">
-    <div class="column is-one-quarter">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-    <div class="column">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    <div class="column is-one-quarter"> Don't know where you want to go? Roll the dice!</div>
+    <div class="column" id="notes"> Your Notes </div>
   </div>
 
 
