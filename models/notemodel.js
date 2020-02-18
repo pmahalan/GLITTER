71a118d3dde05115b194de89c3f91c6727570c6a
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-18 18:25:51 -0500

    revamped structure; installed mongo db, am requiring it, creating Mongo DB models

---
+++ models/notemodel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    synopsis: String,
+
+    destination: { type: String, required: true },
+    season: String,
+    food: String,
+    activities: String,
+    sights: String
+
+  });
+  
+  const Note = mongoose.model("Book", noteSchema);
+  
+  module.exports = Note;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,13 +13,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Add routes, both API and view
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/glitternotes"
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
