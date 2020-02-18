59481ca179b78c446490d6594697e6d09c29c759
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-18 18:47:10 -0500

    added items to seed to the database

--- models/notemodel.js
+++ models/notemodel.js
@@ -2,16 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    synopsis: String,
-
     destination: { type: String, required: true },
     season: String,
     food: String,
     activities: String,
     sights: String
-
   });
   
   const Note = mongoose.model("Book", noteSchema);
