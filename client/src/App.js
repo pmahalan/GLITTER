b978ee5365b2f7c5e51328958eb4a8e33b4e3515
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-17 19:28:53 -0500

    I added text input fields to correspond with each column of the SQL database, as well as a submit button

--- client/src/App.js
+++ client/src/App.js
@@ -18,26 +18,41 @@ function App() {
 
   <div class="columns">
     <div class="column is-one-quarter"> Don't know where you want to go? Roll the dice!</div>
-    <div class="column" id="notes"> Your Notes </div>
+
+    <div class="column" id="note-area"> 
+    
+        <p id="noteslabel"> Your Notes </p>
+
+        <p id="aspect"> Destination </p>
+        <input class="form-control" type="text" id="place" placeholder="Where woud you go right now if you could?"></input>
+
+        <br></br>
+
+        <p id="aspect"> Time of Year </p>
+        <input class="form-control" type="text" id="timeofyear" placeholder="Summer, Spring, Fall, Winter, or all four?"></input>
+
+        <br></br>
+
+        <p id="aspect"> Best Noms </p>
+        <input class="form-control" type="text" id="cuisine" placeholder="Sushi? Poutine? Tiramisu? Escargo?"></input>
+
+        <br></br>
+
+        <p id="aspect"> Things to do </p>
+        <input class="form-control" type="text" id="thingstodo" placeholder="Spa day? Catch up with friends? Museum tours?"></input>
+
+        <br></br>
+
+        <p id="aspect"> I spy... </p>
+        <input class="form-control" type="text" id="landmarks" placeholder="Volcanoes, pyramids, Eiffel Tower, Taj Mahal..."></input>
+
+        <br></br>        
+
+        <button id="note-submit" class="button">Submit!</button>
+
+    </div>
+
   </div>
-
-
-{/* <button class="button">
-  Button
-</button>
-
-<button class="button is-primary">
-  Primary button
-</button>
-
-<button class="button is-large">
-  Large button
-</button>
-
-<button class="button is-loading">
-  Loading button
-</button> */}
-
 
 </div>
   );
