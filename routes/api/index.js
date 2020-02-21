19f48b446fcb235ef1d90247a6812773f571c87e
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-21 13:07:56 -0500

    added a users.js route under the API routes in the routes directory

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const noteRoutes = require("./notes");
+const userRoutes = require("./users");
 
 // Note routes
 router.use("/notes", noteRoutes);
+//User Routes
+router.use("/users", userRoutes);
+// For anything else, render the html page
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  });
 
 module.exports = router;
