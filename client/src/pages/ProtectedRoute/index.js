4e4b04434d801c6146e92c45bf0e7c6890a62a58
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-24 15:17:32 -0500

    made second array of life advice and got in on the page

--- client/src/pages/ProtectedRoute/index.js
+++ client/src/pages/ProtectedRoute/index.js
@@ -252,12 +252,64 @@ function Notes() {
 //THIS IS THE FUNCTION FOR THE LIFE ADVICE ARRAY.
 function GetSecondValue() {
   var mysecondarray= new Array(
+    "Try a new breakfast recipe.",
+    "Do whatever makes you feel good.",
+    "Drink more water.",
+    "Walk everywhere.",
+    "Make a list of everything you’re excited about.",
+    "Start a new passion project.",
+    "Call all of the people you love.",
+    "Buy yourself flowers.",
+    "Learn a party trick.",
+    "Find a rainbow.",
+    "Go to a fortune teller or psychic.",
+    "Buy a back scratcher.",
+    "Write down all the things you feel lucky for.",
+    "Go to the movies by yourself.",
+    "Post a few selfies.",
+    "Work on your posture.",
+    "Be nice.",
+    "Friends night out (or in).",
+    "Stand up for yourself.",
+    "Ask for what you want.",
+    "Get. Stuff. Done.",
+    "Give yourself a pep talk.",
+    "Make your own scavenger hunt",
+    "Order two desserts",
+    "Check out that antique shop you always drive by",
+    "Road trip to a weird diner",
+    "Start decorating your future home",
+    "Write down all your dreams",
+    "Look on the bright side",
+    "Plant a lemon tree for future lemonade",
+    "Watch a movie on VHS",
+    "Listen to your favorite song from middle school",
+    "Go to a drive-in movie",
+    "Watch the sunrise",
+    "Read through old text messages",
+    "Take a walk in a new neighborhood",
+     "Set aside some time to meditate",
+    "Take a mental health day",
+    "Say no to that thing you don’t want to do",
+    "Apply for your dream job",
+    "Set a goal for the day and then do it",
+    "De-clutter",
+    "Open that bottle of wine you were saving",
+    "Treat yourself to a massage",
+    "Get some fresh air",
+    "Make a super chill playlist",
+    "Stay in pajamas all day",
+    "Yes, dye your hair that color",
+    "Order your pie à la mode",
+    "Let someone know how happy they make you",
+    "Start a new holiday tradition",
+    "Tip your waiter a little more than usual"  
     
   );
 
   var random = mysecondarray[Math.floor(Math.random() * mysecondarray.length)];
   //alert(random);
-  document.getElementById("message").innerHTML=random;
+  document.getElementById("secondmessage").innerHTML=random;
 }
 
     return (
@@ -290,12 +342,12 @@ function GetSecondValue() {
           <div id="randomclicker2">
                 <p id="regularmessage"> Spontaneous Life Advice... </p>
                 <input 
-                id="btnSearch" 
-                type="button" 
+                id="secondbtnSearch" 
+                type="secondbutton" 
                 value="Spontaneous Life Advice"
                 onClick={GetSecondValue} />
                 <br></br>
-                <p id="message" ></p>
+                <p id="secondmessage" ></p>
                 <br></br>
             </div>
 
